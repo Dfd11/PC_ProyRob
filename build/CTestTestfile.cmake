e678f8b541e8047b6d791d9584ae7af8a700306a
David Flores <a01209414@itesm.mx>
2022-04-27 17:42:59 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/dfd/PC_ProyRob/src
+# Build directory: /home/dfd/PC_ProyRob/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("roboclaw_driver")
+subdirs("control")
